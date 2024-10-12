797c1c79788f6f19b372f8f8e982e2658e6d264b
LucasChan <chanconan@gmail.com>
2024-10-12 17:10:57 +0800

    black

--- llama_vision.py
+++ llama_vision.py
@@ -1,5 +1,5 @@
 import torch
-from transformers import MllamaForConditionalGeneration, AutoProcessor
+from transformers import AutoProcessor, MllamaForConditionalGeneration
 
 from utils import get_available_gpu_idx
 
@@ -9,41 +9,53 @@ if available_gpu_index is None:
 
 available_cuda = f"cuda:{available_gpu_index}"
 
+
 class LlamaVision:
     def __init__(self):
-        self.device = torch.device(available_cuda if torch.cuda.is_available() else "cpu")
+        self.device = torch.device(
+            available_cuda if torch.cuda.is_available() else "cpu"
+        )
         self.model_id = "meta-llama/Llama-3.2-11B-Vision-Instruct"
         self.model = MllamaForConditionalGeneration.from_pretrained(
-            self.model_id,
-            torch_dtype=torch.bfloat16,
-            low_cpu_mem_usage=True
+            self.model_id, torch_dtype=torch.bfloat16, low_cpu_mem_usage=True
         ).to(self.device)
         self.processor = AutoProcessor.from_pretrained(self.model_id)
-    
-    def generate(self, prompt: str, image=None, max_new_token=1500, temperature=0.8) -> str:
+
+    def generate(
+        self, prompt: str, image=None, max_new_token=1500, temperature=0.8
+    ) -> str:
         """
         Generate text based on the prompt and optional image input.
         """
         prompt_template = "<|begin_of_text|>{}".format(prompt)
-        
-        if image is not None:
-            prompt_template  = "<|image|>" + prompt_template
-         
-        inputs = self.processor(image, prompt_template, return_tensors="pt").to(self.device)
 
-        output = self.model.generate(**inputs, do_sample=True, max_new_tokens=max_new_token, temperature=temperature, top_p=0.9)
-        
-        result = self.processor.decode(output[0])[len(prompt_template) : ]
-        
+        if image is not None:
+            prompt_template = "<|image|>" + prompt_template
+
+        inputs = self.processor(image, prompt_template, return_tensors="pt").to(
+            self.device
+        )
+
+        output = self.model.generate(
+            **inputs,
+            do_sample=True,
+            max_new_tokens=max_new_token,
+            temperature=temperature,
+            top_p=0.9,
+        )
+
+        result = self.processor.decode(output[0])[len(prompt_template) :]
+
         return result
 
     def __call__(self, prompt: str, image=None, max_new_token=1500, temperature=0.8):
         self.__call__.__doc__ = self.generate.__doc__
         return self.generate(prompt, image, max_new_token, temperature)
 
+
 if __name__ == "__main__":
     llm = LlamaVision()
-    
+
     res = llm("What is HKUST? Explain it with short and clear descriptions.")
-    
+
     print(res)
