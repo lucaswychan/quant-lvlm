9ab317b614313a93bc8cff9a3111962a8d85fc7f
LucasChan <chanconan@gmail.com>
2024-10-14 23:33:17 +0800

    add delete page and add docs

--- notion.py
+++ notion.py
@@ -2,8 +2,10 @@ import os
 from notion_client import Client
 from dotenv import load_dotenv
 import json
+from datetime import datetime
 
 load_dotenv()
+
 
 class NotionClient:
     def __init__(self):
@@ -19,33 +21,66 @@ class NotionClient:
                 "Please provide a valid NOTION_PAGE_ID in your environment variables."
             )
         self._client = Client(auth=self._token)
-    
+
     def create_page(self, title: str) -> dict:
-        # create a new page object
+        """
+        Create a new page in the main page.
+        """
+        already_exists, page_id = self._check_subpage_exists(title)
+        if already_exists:
+            return self._client.pages.retrieve(page_id)
+
         new_created_page = self._client.pages.create(
             parent={"page_id": self._main_page_id},
-            properties={
-                "title": [{"text": {"content": title}}]
-            }
+            properties={"title": [{"text": {"content": title}}]},
         )
-        
+
         return new_created_page
 
-    def add_element(self, page_id: str, content_type: str, content_text: str):
-        element = [{
-            "object": "block",
-            "type": content_type,
-            content_type: {
-                "rich_text": [{"type": "text", "text": {"content": content_text}}]
+    def add_element(self, page_id: str, content_type: str, content_text: str) -> None:
+        """ """
+        element = [
+            {
+                "object": "block",
+                "type": content_type,
+                content_type: {
+                    "rich_text": [{"type": "text", "text": {"content": content_text}}]
+                },
             }
-        }]
-        self._client.blocks.children.append(
-            block_id=page_id,
-            children=element
-        )
-        
-        
-        
+        ]
+        self._client.blocks.children.append(block_id=page_id, children=element)
+
+    def _check_subpage_exists(self, subpage_title) -> tuple[bool, str]:
+        try:
+            # Search for pages with the given title
+            response = self._client.search(
+                query=subpage_title, filter={"property": "object", "value": "page"}
+            ).get("results")
+
+            # Filter the results to find a page with matching parent and title
+            for page in response:
+                if (
+                    page.get("parent", {}).get("type") == "page_id"
+                    and page["parent"]["page_id"].replace("-", "") == self._main_page_id
+                ):
+                    # Check the title of the page
+                    page_title = page["properties"].get("title", {}).get("title", [])
+                    if page_title and page_title[0].get("plain_text") == subpage_title:
+                        return True, page["id"]
+
+            # If no matching page is found
+            return False, None
+
+        except Exception as e:
+            print(f"An error occurred while checking for the subpage: {str(e)}")
+            return False, None
+
+    def delete_page(self, title: str) -> None:
+        """
+        Not really deleting the page, but archiving it.
+        """
+        self._client.pages.update(self._check_subpage_exists(title)[1], archived=True)
+
 
 # properties_to_update = {
 #     "title": {"title": [{"text": {"content": "Finance News"}}]},
@@ -63,7 +98,7 @@ class NotionClient:
 
 # with open("notion_data.json", "w") as f:
 #     json.dump(existing_blocks, f, indent=4)
-    
+
 # new_content = [
 #     {
 #         "object": "block",
@@ -118,11 +153,6 @@ class NotionClient:
 if __name__ == "__main__":
     notion = NotionClient()
     tickers_news = None
-    new_page = notion.create_page("Finance News Sub Page")
-    
-    
-    new_page_id = new_page["id"]
-    
-    notion.add_element(new_page_id, "paragraph", "This is a new paragraph.")
-    notion.add_element(new_page_id, "heading_2", "This is a new heading")
-    notion.add_element(new_page_id, "bulleted_list_item", "This is a bullet point")
+    today = datetime.today().strftime("%m-%d-%Y")
+    print(today)
+    # notion.delete_page(f"cde - Finance News")
